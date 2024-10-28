d862a1c34a2d6722b3395e4c0c14467ee756c9af
vu-long-duong <duonglongvujvcr@gmail.com>
2024-10-28 17:38:42 +0700

    28/10 commit 1

--- theme/frontend/js/main.js
+++ theme/frontend/js/main.js
@@ -37,23 +37,96 @@ WEBS = (function () {
     }
 
     var menuMobile = function () {
-        var btnMenu = document.querySelector('.header-top-mobile-head__menu');
-        var btnClose = document.querySelector('.header-top-mobile-group-top__close');
-        var mobileGroup = document.querySelector('.header-top-mobile-group');
+        var btnMenu = document.querySelector('.header-logo__menu');
+        var btnClose = document.querySelector('.header-logo__close');
+        var wrapper = document.querySelector('.wrapper');
+        var wrapperGroup = document.querySelector('.header-logo-group');
 
         btnMenu.addEventListener('click', function() {
-            mobileGroup.classList.add('header-top-mobile-group-active');
+          wrapper.classList.add('show');
         });
 
         btnClose.addEventListener('click', function() {
-            mobileGroup.classList.remove('header-top-mobile-group-active');
+          wrapper.classList.remove('show');
+        } );
+      
+        document.addEventListener('click', (event) => {
+          if (!wrapperGroup.contains(event.target) && !btnMenu.contains(event.target)) {
+            wrapper.classList.remove('show');
+          }
+        } );
+      
+      // nav
+      var headerLogoNav = document.querySelector('.header-logo-nav');
+      var listItems = headerLogoNav.querySelectorAll( 'li:has(ul)' );
+
+      var previousElement = null;
+        if (listItems.length <= 0) {
+          return;
+        }
+    
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].addEventListener( "click", function ()
+            {
+            var parentAccItemHeader = this.parentElement;
+    
+            if (parentAccItemHeader) {
+              parentAccItemHeader.classList.toggle(
+                "header-logo-nav--active"
+              );
+    
+              if (previousElement === this) {
+                return;
+              }
+    
+              if (previousElement) {
+                previousElement.parentElement.classList.remove(
+                  "header-logo-nav--active"
+                );
+              }
+    
+              previousElement = this;
+            }
+          });
+        }
+      
+    }
+  
+    var runNumber = function () {
+      var counters = document.querySelectorAll('.general-content-list-item__number');
+      
+      counters.forEach(counter => {
+        var target = +counter.getAttribute('data-target');
+  
+        var animate = () => {
+          var current = +counter.textContent.replace('+', '');
+  
+          var formattedCurrent = current < 10 ? `0${current}` : current;
+          if (current < target) {
+            counter.textContent = `${formattedCurrent + 1}+`;
+            requestAnimationFrame(animate);
+          } else {
+            counter.textContent = `${target < 10 ? `0${target}` : target+"+"}`;
+          }
+        };
+  
+        var observer = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              animate();
+              observer.unobserve(counter);
+            }
+          });
         });
+  
+        observer.observe(counter);
+      });
     }
   
     var firework = function ()
     {
-      const banner = document.querySelector('.banner');
-      const firework = document.createElement('div');
+      var banner = document.querySelector('.banner');
+      var firework = document.createElement('div');
       firework.className = 'banner-firework';
       firework.style.left = Math.random() * 100 + '%';
       banner.appendChild(firework);
@@ -63,28 +136,57 @@ WEBS = (function () {
       }, 2000);
     }
 
-    var slideSlick = function ( className )
-    {
-        $(document).ready(function(){
-            $(className).slick({
-                dots: true,
-                infinite: false,
-                speed: 500,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplay: false,
-                autoplaySpeed: 2000,
-                arrows: true,
-                prevArrow: false,
-                nextArrow: false,
-
-            });
-        });
-    }
+    var slideSlick = function (classNames) {
+      // Split the classNames parameter by commas
+      classNames = classNames.split(',');
+  
+      // Wait for the document to be ready
+      $(document).ready(function () {
+          classNames.forEach(className => {
+              if (className.trim() === ".leader-list") {
+                $(className).slick({
+                    dots: true,
+                    infinite: false,
+                    speed: 500,
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    autoplay: false,
+                    autoplaySpeed: 2000,
+                    arrows: true,
+                    prevArrow: false,
+                    nextArrow: false,
+                    responsive: [
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2,
+                            }
+                        }
+                    ],
+                });
+              }
+              else
+              {
+                $(className).slick({
+                  dots: true,
+                  infinite: false,
+                  speed: 500,
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  autoplay: false,
+                  autoplaySpeed: 2000,
+                  arrows: true,
+                  prevArrow: false,
+                  nextArrow: false,
+              });
+            }
+          });
+      });
+  }
 
     var scrollHeader = function() {
         var header = document.querySelector('header');
-        var headerTop = document.querySelector('.header-top');
         var headerHeight = header.offsetHeight;
         var isFixed = false;
 
@@ -93,7 +195,7 @@ WEBS = (function () {
             var shouldBeFixed = window.scrollY > headerHeight;
             if (shouldBeFixed !== isFixed) {
                 isFixed = shouldBeFixed;
-                headerTop.classList.toggle('fixed', isFixed);
+                header.classList.toggle('fixed', isFixed);
             }
         });
     }
@@ -103,7 +205,7 @@ WEBS = (function () {
       document.querySelector('.banner-body-view__icon').addEventListener('click', function (event) {
         event.preventDefault();
     
-        const videoContainer = document.getElementById('banner-video');
+        var videoContainer = document.getElementById('banner-video');
     
         videoContainer.innerHTML = `
             <iframe width="auto" height="auto" src="https://www.youtube.com/embed/Vj_5mYUQjzk" title="VIETIS President Sharing" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -123,12 +225,13 @@ WEBS = (function () {
 
     return {
         _: function () {
-            // scrollHeader();
+            scrollHeader();
             ifameAction();
             menuMobile();
             slideSlick(".slide-main-list , .leader-list");
             menuAccordion();
-            setInterval(firework, 2000);
+            runNumber();
+            // setInterval(firework, 2000);
         }
     };
 })();
